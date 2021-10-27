510acc46c32beb8570a462a38fe1c5f97bf3972f
Wonyeong Choi <coma01155@gmail.com>
2021-10-27 13:25:46 +0900

    add eslint rule
    remove prop-types rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,6 @@ module.exports = {
     'react/jsx-uses-react': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': 0,
   },
 };
