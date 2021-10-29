d89ca10e566d4f50f260b8bdfca5e0503f1872ec
Wonyeong Choi <coma01155@gmail.com>
2021-10-29 12:07:05 +0900

    반응형 디자인 적용

--- src/Components/Event/EventCard.js
+++ src/Components/Event/EventCard.js
@@ -6,12 +6,34 @@ import { EyeOutlined, HeartOutlined, HeartTwoTone } from '@ant-design/icons';
 import { useState, useCallback } from 'react';
 
 const CardStyle = styled(Card)`
-  width: 75em;
   margin-top: 30px;
   margin-bottom: 30px;
   text-align: center;
   display: flex;
   justify-self: center;
+  max-width: 1100px;
+
+  @media screen and (min-width: 425px) {
+    width: 360px;
+    display: block;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 700px;
+    display: flex;
+  }
+  
+  @media screen and (min-width: 1196px) {
+    width: 1100px;
+    display: flex;
+  }
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
 `;
 
 function EventCard({ eventPost }) {
@@ -36,18 +58,12 @@ function EventCard({ eventPost }) {
 )}
       bodyStyle={{ width: '100%' }}
     >
-      <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        width: '100%',
-        justifyContent: 'center',
-      }}
-      >
+      <Wrapper>
         <Row align="middle">
-          <Col span={4} style={{ textAlign: 'left' }}>
+          <Col span={12} style={{ textAlign: 'left' }}>
             {eventPost.date}
           </Col>
-          <Col span={4} offset={16}>
+          <Col span={12} style={{ textAlign: 'right' }}>
             price:
             {eventPost.price}
           </Col>
@@ -63,19 +79,19 @@ function EventCard({ eventPost }) {
           </Col>
         </Row>
         <Row align="middle">
-          <Col span={4} style={{ textAlign: 'left' }}>
+          <Col span={12} style={{ textAlign: 'left' }}>
             <Avatar src={eventPost.avatar} />
             {' '}
             {eventPost.channelName}
           </Col>
-          <Col span={4} offset={16}>
+          <Col span={12} style={{ textAlign: 'right' }}>
             <EyeOutlined />
             {eventPost.view}
             {' '}
             {liked ? <HeartTwoTone onClick={onToggleLike} twoToneColor="#eb2f96" /> : <HeartOutlined onClick={onToggleLike} />}
           </Col>
         </Row>
-      </div>
+      </Wrapper>
     </CardStyle>
   );
 }
--- src/Routes/Category/CategoryPresenter.js
+++ src/Routes/Category/CategoryPresenter.js
@@ -1,4 +1,4 @@
-import { Pagination } from 'antd';
+import { Divider, Pagination } from 'antd';
 import styled from 'styled-components';
 import { useCallback, useState } from 'react';
 import Header from '../../Components/Header';
@@ -6,15 +6,27 @@ import Footer from '../../Components/Footer';
 import EventCard from '../../Components/Event/EventCard';
 
 const EventTitle = styled.p`
-  width: 70em;
   font-family: Roboto;
   font-style: normal;
   font-weight: normal;
   font-size: 15px;
   color: #5C3FBF;
+  text-decoration: underline;
   justify-self: flex-start;
   letter-spacing: 0.56px;
   padding-top: 64px;
+  max-width: 1100px;
+  @media screen and (min-width: 425px) {
+    width: 360px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 700px;
+  }
+
+  @media screen and (min-width: 1196px) {
+    width: 1100px;
+  }
 `;
 
 const Wrapper = styled.div`
@@ -22,6 +34,25 @@ const Wrapper = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const DividerWrapper = styled(Divider)`
+  width: 1100px;
+  min-width: auto;
+  @media screen and (min-width: 425px) {
+    width: 360px;
+    min-width: auto;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 700px;
+    min-width: auto;
+  }
+
+  @media screen and (min-width: 1196px) {
+    width: 1100px;
+    min-width: auto;
+  }
 `;
 
 function CategoryPresenter({ mainEvents, type }) {
@@ -44,6 +75,7 @@ function CategoryPresenter({ mainEvents, type }) {
       <Header />
       <Wrapper>
         <EventTitle>{mappingType()}</EventTitle>
+        <DividerWrapper />
         {mainEvents
           .slice((pageNumber - 1) * 5, Math.min(mainEvents.length, pageNumber * 5))
           .map((event) => <EventCard key={event.id} eventPost={event} />)}
