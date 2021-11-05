e7855ee0d7cd4a292a30534827f4d702b4bae117
Wonyeong Choi <coma01155@gmail.com>
2021-11-05 11:55:37 +0900

    css style 수정

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,33 +11,25 @@ const HeaderBackground = styled.div`
   background-color: white;
 `;
 
-const SignInButton = styled.button`
-  margin-left: 680px;
-  padding: 0 20px;
-  width:90px;
-  height: 32px;
-  background: #FAF8FF;
-  border-radius: 16px;
-  border: none;
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 11px;
-  line-height: 32px;
-  text-align: center;
-  letter-spacing: 0.42px;
-  color: #5C3FBF;
-  &:hover{
-        color:white;
-        background-color:#5C3FBF;
-    }
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+  
+  div:first-of-type{
+    flex-grow: 3;
+  }
+
+  div:last-of-type{
+    display: flex;
+    justify-content: space-evenly;
+    flex-grow: 1
+  }
 `;
 
-const LoginButton = styled.button`
-  width:90px;
+const ButtonWrapper = styled.button`
   height: 32px;
   padding: 0 20px;
-  margin-left: 16px;
   background: #FAF8FF;
   border-radius: 16px;
   border: none;
@@ -50,31 +42,10 @@ const LoginButton = styled.button`
   letter-spacing: 0.42px;
   color: #5C3FBF;
   &:hover{
-        color:white;
-        background-color:#5C3FBF;
-    }
-`;
-
-const LogoutButton = styled.button`
-  padding: 0 20px;
-  margin-left: 16px;
-  width:90px;
-  height: 32px;
-  background: #FAF8FF;
-  border-radius: 16px;
-  border: none;
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 11px;
-  line-height: 32px;
-  text-align: center;
-  letter-spacing: 0.42px;
-  color: #5C3FBF;
-  &:hover{
-        color:white;
-        background-color:#5C3FBF;
-    }
+    color:white;
+    background-color:#5C3FBF;
+    cursor: pointer;
+  }
 `;
 
 const HomeButton = styled.button`
@@ -130,18 +101,25 @@ function GoHome() {
 
 const Header = () => (
   <HeaderBackground>
-    <HomeButton onClick={GoHome}>
-      <img alt="logo" src="img/logo.png" />
-    </HomeButton>
-    <SignInButton onClick={GoSignin}>
-      회원가입
-    </SignInButton>
-    <LoginButton onClick={GoLogin}>
-      로그인
-    </LoginButton>
-    <LogoutButton onClick={GoLogout}>
-      로그아웃
-    </LogoutButton>
+    <Wrapper>
+      <div>
+        <HomeButton onClick={GoHome}>
+          <img alt="logo" src="img/logo.png" />
+        </HomeButton>
+      </div>
+      <div>
+        <ButtonWrapper>호스트센터</ButtonWrapper>
+        <ButtonWrapper onClick={GoSignin}>
+          회원가입
+        </ButtonWrapper>
+        <ButtonWrapper onClick={GoLogin}>
+          로그인
+        </ButtonWrapper>
+        <ButtonWrapper onClick={GoLogout}>
+          로그아웃
+        </ButtonWrapper>
+      </div>
+    </Wrapper>
     <NavList>
       <NavItem to="/events">전체</NavItem>
       <NavItem to={{ pathname: '/category', search: '?type=sushi' }}>수시행사</NavItem>
