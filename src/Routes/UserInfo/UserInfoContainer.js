755ea1d83a1ee676c93eb9c8028569c584dba0b4
ssumthingood <ssumthingood@gmail.com>
2021-11-06 01:38:30 +0900

    UserInfo 상태관리 Hooks사용해서 대충 완성

--- src/Routes/UserInfo/UserInfoContainer.js
+++ src/Routes/UserInfo/UserInfoContainer.js
@@ -1,8 +1,55 @@
+import { useState } from 'react';
 import UserInfoPresenter from './UserInfoPresenter';
 
 function UserInfoContainer() {
+  const [myName, setName] = useState('');
+
+  const [myPw, setPw] = useState('');
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+    console.log(myName);
+  };
+
+  const onChangePw = (e) => {
+    setPw(e.target.value);
+    console.log(myPw);
+  };
+
+  function Save() {
+    alert('저장되었습니다.');
+    console.log('Saved');
+    window.location.assign('/home');
+  }
+
+  function ChangeName() {
+    console.log(myName);
+    alert('변경되었습니다.');
+  }
+
+  function ChangePw() {
+    console.log(myPw);
+    alert('변경되었습니다.');
+  }
+
+  function DontGo() {
+    alert('어딜 갈라고 그래');
+    console.log('DontGo');
+  }
+
   return (
-    <UserInfoPresenter />
+    <UserInfoPresenter
+      Save={Save}
+      ChangeName={ChangeName}
+      ChangePw={ChangePw}
+      DontGo={DontGo}
+      myName={myName}
+      myPw={myPw}
+      setId={setName}
+      setPw={setPw}
+      onChangeName={onChangeName}
+      onChangePw={onChangePw}
+    />
   );
 }
 
