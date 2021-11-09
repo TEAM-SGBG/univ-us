ae7a2ff7d7d9474e9b9401424057d096d2f2646d
Wonyeong Choi <coma01155@gmail.com>
2021-11-09 21:32:13 +0900

    feat: 채널 생성 기능 완성

--- src/Components/HostCenter/CreateChannel/CreateChannelForm.js
+++ src/Components/HostCenter/CreateChannel/CreateChannelForm.js
@@ -1,10 +1,23 @@
-import { useState } from 'react';
-import { Form, Input, Button } from 'antd';
+import { useState, useEffect } from 'react';
+import {
+  Form, Input, Button, Modal, Space,
+} from 'antd';
 import styled from 'styled-components';
-import useInput from '../../../Hooks/useInput';
+import { useHistory } from 'react-router';
 
 const InputWrapper = styled(Input)`
   height: 48px;
+  
+  :hover{
+    border-color: #5C3FBF;
+  }
+  
+  :focus {
+    border-color: #5C3FBF;
+    outline: 0;
+    -webkit-box-shadow: 0 0 0 2px rgba(92, 63, 191, .2);
+    box-shadow: 0 0 0 2px rgba(92, 63, 191, .2);
+  }
 `;
 
 const ButtonWrapper = styled(Button)`
@@ -12,20 +25,92 @@ const ButtonWrapper = styled(Button)`
   height: 48px;
   background: #FAF8FF;
   border: 0;
-  cursor: pointer;
+  cursor: 'pointer';
   color: #5C3FBF;
+  
+  :hover{
+    background: #FAF8FF;
+    color: #5C3FBF;
+  }
+  
+  :active{
+    background: #FAF8FF;
+    color: #5C3FBF;
+  }
+
+  :focus{
+    background: #FAF8FF;
+    color: #5C3FBF;
+  }
 `;
 
 const CreateChannelForm = () => {
-  const [channelName, onChangeChannelName] = useInput('');
-  const [channelID, onChangeChannelID] = useInput('');
+  const history = useHistory();
+
+  const [channelName, setChannelName] = useState({ value: '' });
+  const [channelID, setChannelID] = useState({ value: '' });
+
   const [form] = Form.useForm();
+
   const [validate, setValidate] = useState(false);
   const [duplicateCheck, setDuplicateCheck] = useState(false);
   const [duplicateCheckLoading, setDuplicateCheckLoading] = useState(false);
 
-  const onFinish = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
+  const handleOk = () => {
+    setIsModalVisible(false);
+    history.push('/hostcenter/channel');
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const validateName = (name) => {
+    const nameRegex = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]{1,20}$/;
+
+    if (nameRegex.test(name)) {
+      return {
+        validateStatus: 'success',
+        errorMsg: null,
+      };
+    }
+
+    return {
+      validateStatus: 'error',
+      errorMsg: '한글/영어/숫자만 입력할 수 있습니다.',
+    };
+  };
+
+  const validateID = (id) => {
+    const idRegex = /^[a-z|A-Z|0-9|]{1,20}$/;
+
+    if (idRegex.test(id)) {
+      return {
+        validateStatus: 'success',
+        errorMsg: null,
+      };
+    }
+
+    return {
+      validateStatus: 'error',
+      errorMsg: '영어/숫자만 입력할 수 있습니다.',
+    };
+  };
+
+  const onChangeChannelID = (e) => {
+    setChannelID({ ...validateID(e.target.value), value: e.target.value });
+  };
+
+  const onChangeChannelName = (e) => {
+    setChannelName({ ...validateName(e.target.value), value: e.target.value });
+  };
+
+  const onFinish = () => {
+    if (validate) {
+      setIsModalVisible(true);
+    }
   };
 
   const onFinishFailed = () => {
@@ -36,11 +121,20 @@ const CreateChannelForm = () => {
     if (duplicateCheck) return;
     setDuplicateCheckLoading(true);
     setTimeout(() => {
-      setValidate(true);
       setDuplicateCheck(true);
       setDuplicateCheckLoading(false);
     }, 500);
   };
+
+  useEffect(() => {
+    setDuplicateCheck(false);
+  }, [channelID]);
+
+  useEffect(() => {
+    setValidate(channelName.validateStatus === 'success'
+        && channelID.validateStatus === 'success'
+        && duplicateCheck);
+  }, [channelName, channelID, duplicateCheck]);
 
   return (
     <Form
@@ -54,25 +148,33 @@ const CreateChannelForm = () => {
       <Form.Item
         label="채널 이름"
         name="ChannelName"
+        validateStatus={channelName.validateStatus}
+        help={channelName.errorMsg}
         required
       >
         <InputWrapper
           value={channelName}
           onChange={onChangeChannelName}
-          maxLength={50}
+          placeholder="20자 이내 (한글/영문/숫자)"
+          maxLength={20}
         />
       </Form.Item>
 
       <Form.Item
         style={{ display: 'flex' }}
         label="채널 ID"
-        name="ChannelID"
         required
       >
-        <Form.Item style={{ display: 'inline-block', width: '70%' }}>
+        <Form.Item
+          name="ChannelID"
+          validateStatus={channelID.validateStatus}
+          help={channelID.errorMsg}
+          style={{ display: 'inline-block', width: '70%' }}
+        >
           <InputWrapper
             value={channelID}
             onChange={onChangeChannelID}
+            placeholder="20자 이내 (영문/숫자)"
             maxLength={20}
           />
         </Form.Item>
@@ -83,6 +185,7 @@ const CreateChannelForm = () => {
           <ButtonWrapper
             onClick={isValidate}
             loading={duplicateCheckLoading}
+            disabled={channelID.validateStatus === 'error' || duplicateCheck}
           >
             {duplicateCheck ? '✓' : '중복 확인'}
           </ButtonWrapper>
@@ -93,8 +196,8 @@ const CreateChannelForm = () => {
         wrapperCol={{ span: 24 }}
       >
         <p style={{ textAlign: 'center' }}>
-          채널페이지: https://univ-us.kr/
-          {channelID}
+          채널페이지: https://univ-us.kr
+          {channelID.value ? `/${channelID.value}` : '/{채널 ID}'}
           /event
         </p>
       </Form.Item>
@@ -105,11 +208,24 @@ const CreateChannelForm = () => {
       >
         <ButtonWrapper
           disabled={!validate}
-          htmltype="submit"
+          htmlType="submit"
         >
           개설하기
         </ButtonWrapper>
       </Form.Item>
+      <Modal
+        title="확인"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>
+          <Space>
+            {channelName.value}
+            채널을 개설하시겠습니까?
+          </Space>
+        </p>
+      </Modal>
     </Form>
   );
 };
