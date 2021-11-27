82d22ddc2be3b92357026d35b02da31ef842a6b8
Wonyeong Choi <coma01155@gmail.com>
2021-11-28 01:56:01 +0900

    refactor: 컴포넌트를 넘기도록 수정

--- src/Routes/Category/CategoryContainer.js
+++ src/Routes/Category/CategoryContainer.js
@@ -1,8 +1,39 @@
 import { useLocation } from 'react-router-dom';
 import { useCallback, useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import styled from 'styled-components';
 import CategoryPresenter from './CategoryPresenter';
 import { LOAD_CATEGORY_EVENTS_REQUEST } from '../../reducers/post';
+
+const TitleWrapper = styled.p`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 15px;
+  color: #5C3FBF;
+  text-decoration: underline;
+  justify-self: flex-start;
+  letter-spacing: 0.56px;
+  padding-top: 64px;
+  max-width: 1100px;
+
+  @media screen and (min-width: 0px) {
+    width: 360px;
+    display: block;
+  }
+
+  @media screen and (min-width: 425px) {
+    width: 360px;
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 700px;
+  }
+
+  @media screen and (min-width: 1196px) {
+    width: 1100px;
+  }
+`;
 
 function CategoryContainer() {
   const useQuery = () => new URLSearchParams(useLocation().search);
@@ -19,11 +50,11 @@ function CategoryContainer() {
     loadCategoryEventsError,
   } = useSelector(((state) => state.post));
 
-  const mappingType = useCallback(() => {
-    if (type === 'sushi') return '수시행사';
-    if (type === 'jungshi') return '정시행사';
-    if (type === 'fair') return '대학박람회';
-    return '전체';
+  const EventTitle = useCallback(() => {
+    if (type === 'sushi') return <TitleWrapper>수시행사</TitleWrapper>;
+    if (type === 'jungshi') return <TitleWrapper>정시행사</TitleWrapper>;
+    if (type === 'fair') return <TitleWrapper>대학박람회</TitleWrapper>;
+    return <TitleWrapper>전체</TitleWrapper>;
   }, [type]);
 
   const onChangePageNumber = (v) => {
@@ -40,7 +71,7 @@ function CategoryContainer() {
   return (
     <CategoryPresenter
       events={categoryEvents}
-      mappingType={mappingType}
+      EventTitle={EventTitle}
       onChangePageNumber={onChangePageNumber}
       pageNumber={pageNumber}
       loading={loadCategoryEventsLoading}
--- src/Routes/Category/CategoryPresenter.js
+++ src/Routes/Category/CategoryPresenter.js
@@ -5,36 +5,6 @@ import Header from '../../Components/Header';
 import Footer from '../../Components/Footer';
 import EventList from '../../Components/Event/EventList';
 
-const EventTitle = styled.p`
-  font-family: Roboto;
-  font-style: normal;
-  font-weight: normal;
-  font-size: 15px;
-  color: #5C3FBF;
-  text-decoration: underline;
-  justify-self: flex-start;
-  letter-spacing: 0.56px;
-  padding-top: 64px;
-  max-width: 1100px;
-
-  @media screen and (min-width: 0px) {
-    width: 360px;
-    display: block;
-  }
-
-  @media screen and (min-width: 425px) {
-    width: 360px;
-  }
-
-  @media screen and (min-width: 768px) {
-    width: 700px;
-  }
-
-  @media screen and (min-width: 1196px) {
-    width: 1100px;
-  }
-`;
-
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -68,13 +38,13 @@ const DividerWrapper = styled(Divider)`
 `;
 
 function CategoryPresenter({
-  pageNumber, mappingType, onChangePageNumber, events, loading, done, error,
+  pageNumber, EventTitle, onChangePageNumber, events, loading, done, error,
 }) {
   return (
     <>
       <Header />
       <Wrapper>
-        <EventTitle>{mappingType()}</EventTitle>
+        <EventTitle />
         <DividerWrapper />
         {loading && <LoadingOutlined />}
         {done && (
