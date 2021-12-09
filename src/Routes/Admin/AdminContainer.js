d06141d14d3106a3523372880e4886c3d1d6c264
ssumthingood <ssumthingood@gmail.com>
2021-12-09 23:21:33 +0900

    admin

--- src/Routes/Admin/AdminContainer.js
+++ src/Routes/Admin/AdminContainer.js
@@ -1,17 +1,78 @@
-// import { useState } from 'react';
-// import { useHistory } from 'react-router';
-// import AdminPresenter from './AdminPresenter';
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+import { useSelector } from 'react-redux';
+// import { useDispatch } from 'react-redux';
+import axios from 'axios';
+import AdminPresenter from './AdminPresenter';
+import user from '../../mock/user.json';
+// import { LOAD_SUBSCRIBE_CHANNELS_REQUEST } from '../../reducers/user';
 
 function AdminContainer() {
-  // const history = useHistory();
+  const history = useHistory();
   // const dispatch = useDispatch();
-  // function goOne() {
-  //   setNum('1');
-  // }
+  const {
+    loadSubscribeChannelsLoading,
+    loadSubscribeChannelsDone,
+    loadSubscribeChannelsError,
+    // subscribeChannels,
+  } = useSelector((state) => state.user);
+  const [myNum, setNum] = useState('1');
+  const [channels, setChannels] = useState([]);
+  const [events, setEvents] = useState([]);
+  const [users, setUsers] = useState([]);
 
-  // function goTwo() {
-  //   setNum('2');
-  // }
+  useEffect(() => {
+    axios.get('http://localhost:3001/api/channel/all', { withCredential: 'true' })
+      .then((response) => {
+        setChannels(response.data.data);
+      });
+
+    axios.post('http://localhost:3001/api/events/all_events', { withCredential: 'true' })
+      .then((response) => {
+        setEvents(response.data.data);
+      });
+
+    axios.get('http://localhost:3001/api/mypage/subscribe_info', { withCredential: 'true' })
+      .then((response) => {
+        setUsers(response.data.data);
+      });
+  }, []);
+
+  function goOne() {
+    setNum('1');
+  }
+
+  function goTwo() {
+    setNum('2');
+  }
+
+  function goThree() {
+    // dispatch({ type: LOAD_SUBSCRIBE_CHANNELS_REQUEST });
+    setNum('3');
+  }
+
+  function goUserinfo() {
+    history.push('/userinfo');
+  }
+
+  return (
+    <AdminPresenter
+      goUserinfo={goUserinfo}
+      myNum={myNum}
+      setNum={setNum}
+      goOne={goOne}
+      goTwo={goTwo}
+      goThree={goThree}
+      // channel={subscribeChannels}
+      channels={channels}
+      events={events}
+      users={users}
+      user={user}
+      loading={loadSubscribeChannelsLoading}
+      done={loadSubscribeChannelsDone}
+      error={loadSubscribeChannelsError}
+    />
+  );
 }
 
 export default AdminContainer;
--- src/Routes/Admin/AdminPresenter.js
+++ src/Routes/Admin/AdminPresenter.js
@@ -1,27 +1,27 @@
-// import MiniHeader from 'Components/MiniHeader';
 import Footer from 'Components/Footer';
 import MiniHeader from 'Components/MiniHeader';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
   margin: 0 auto;
-  width: 1200px;
+  width:1200px;
   padding:60px;
   font-family: Roboto;
 `;
 
-const AdminH1 = styled.h1`
+const MyPageH1 = styled.h1`
   font-size:22px;
   line-height:35px;
   display:block;
+  /* margin-bottom:60px; */
 `;
 
-const AdminList = styled.ul`
+const MyPageList = styled.ul`
   display:flex;
   margin-top:25px;
 `;
 
-const AdminItem = styled.li`
+const MyPageItem = styled.li`
   width:125px;
   height: 25px;
   line-height:25px;
@@ -35,18 +35,124 @@ const AdminItem = styled.li`
     }
 `;
 
-function AdminPresenter() {
+const ReservedEvent = styled.div`
+  margin:25px;
+`;
+
+const ReservedTable = styled.table`
+  border:none;
+`;
+
+const ReservedTh1 = styled.th`
+  text-align:center;
+  width: 150px;
+  background-color: whitesmoke;
+  color:#5C3FBF;
+`;
+
+const ReservedTh2 = styled.th`
+  text-align:center;
+  width: 300px;
+  background-color: whitesmoke;
+  color:#5C3FBF;
+`;
+
+const ReservedTh3 = styled.th`
+  text-align:center;
+  width: 700px;
+  background-color: whitesmoke;
+  color:#5C3FBF;
+`;
+
+const ReservedTr = styled.tr`
+  text-align:center;
+`;
+
+const ReservedTd1 = styled.td`
+  width: 150px;
+  height:50px;
+  line-height:50px;
+`;
+const ReservedTd2 = styled.td`
+   width: 300px;
+`;
+const ReservedTd3 = styled.td`
+   width: 700px;
+`;
+
+const LikedEvent = styled.div`
+  margin:25px;
+`;
+
+function AdminPresenter({
+  myNum, goOne, goTwo, goThree, channels, events, users,
+}) {
   return (
     <>
       <MiniHeader />
       <Wrapper>
-        <AdminH1>
-          관리자
-        </AdminH1>
-        <AdminList>
-          <AdminItem index="1">회원 관리</AdminItem>
-          <AdminItem index="2">채널 관리</AdminItem>
-        </AdminList>
+        <MyPageH1>
+          마이페이지
+        </MyPageH1>
+        <MyPageList>
+          <MyPageItem index="1" onClick={goOne}>채널 관리</MyPageItem>
+          <MyPageItem index="2" onClick={goTwo}>행사 삭제</MyPageItem>
+          <MyPageItem index="3" onClick={goThree}>회원 삭제</MyPageItem>
+        </MyPageList>
+        {(myNum === '1') && (
+        <ReservedEvent>
+          <ReservedTable>
+            <ReservedTr>
+              <ReservedTh1>신청일</ReservedTh1>
+              <ReservedTh2>신청행사</ReservedTh2>
+              <ReservedTh3>상세내용</ReservedTh3>
+            </ReservedTr>
+            {channels.map((channel) => (
+              <ReservedTr key={1}>
+                <ReservedTd1>{channel.id}</ReservedTd1>
+                <ReservedTd2>{channel.title}</ReservedTd2>
+                <ReservedTd3>{channel.name}</ReservedTd3>
+              </ReservedTr>
+            ))}
+          </ReservedTable>
+        </ReservedEvent>
+        )}
+        {(myNum === '2') && (
+          <LikedEvent>
+            <ReservedTable>
+              <ReservedTr>
+                <ReservedTh1>신청일</ReservedTh1>
+                <ReservedTh2>신청행사</ReservedTh2>
+                <ReservedTh3>상세내용</ReservedTh3>
+              </ReservedTr>
+              {events.map((event) => (
+                <ReservedTr key={1}>
+                  <ReservedTd1>{event.id}</ReservedTd1>
+                  <ReservedTd2>{event.title}</ReservedTd2>
+                  <ReservedTd3>{event.date}</ReservedTd3>
+                </ReservedTr>
+              ))}
+            </ReservedTable>
+          </LikedEvent>
+        )}
+        {(myNum === '3') && (
+          <LikedEvent>
+            <ReservedTable>
+              <ReservedTr>
+                <ReservedTh1>신청일</ReservedTh1>
+                <ReservedTh2>신청행사</ReservedTh2>
+                <ReservedTh3>상세내용</ReservedTh3>
+              </ReservedTr>
+              {users.map((user) => (
+                <ReservedTr key={1}>
+                  <ReservedTd1>{user.id}</ReservedTd1>
+                  <ReservedTd2>{user.title}</ReservedTd2>
+                  <ReservedTd3>{user.date}</ReservedTd3>
+                </ReservedTr>
+              ))}
+            </ReservedTable>
+          </LikedEvent>
+        )}
       </Wrapper>
       <Footer />
     </>
